cb3da2ad3dc5b9ab3a02ea79ad0c49110cf8a845
luisxcam <luisxcam@gmail.com>
2015-06-10 21:35:04 -0400

    Added content to master page. Starting default page

--- COMP2007-Assignment01/default.aspx.designer.cs
+++ COMP2007-Assignment01/default.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace COMP2007_Assignment01
 
     public partial class _default
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
