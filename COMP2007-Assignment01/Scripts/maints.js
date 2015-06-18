e1016b6f34f8d7b750ed681a7545e2d8e0e74d83
luisxcam <luisxcam@gmail.com>
2015-06-18 02:30:41 -0400

    Merging

---
+++ COMP2007-Assignment01/Scripts/maints.js
@@ -0,0 +1,8 @@
+/*How to capture the values. Is necessary to modify the textbox to ClientIDMode="Static"
+ * var str = document.getElementById('PointsScored01TextBox').value;
+ * console.log(str);
+/*How to capture the values. Is necessary to modify the textbox to ClientIDMode="Static"
+ * var str = document.getElementById('PointsScored01TextBox').value;
+ * console.log(str);
+ 
+//# sourceMappingURL=maints.js.map
